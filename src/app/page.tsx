e25a25086ebd09739ba05f8fe65e39a9d5b467d0
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-07-27 19:12:24 -0300

    feat: change homepage metadata and remove useless content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import { UserButton } from '@clerk/nextjs';
-import { SignIn } from '@clerk/nextjs';
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <UserButton afterSignOutUrl="/" />
-      </div>
-      <SignIn />
-    </main>
-  );
+  return <main>Mikasa</main>;
 }
