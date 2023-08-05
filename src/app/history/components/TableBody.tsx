ff5438bbf9762c6e0edf8b6d368b345c0840eb74
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-08-05 19:02:39 -0300

    feat: order tasks by most recent

--- src/app/history/components/TableBody.tsx
+++ src/app/history/components/TableBody.tsx
@@ -3,34 +3,66 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
+import { Cycle, useCyclesStore } from '@/stores/cycles';
+
 dayjs.extend(relativeTime);
 
-import { useCyclesStore } from '@/stores/cycles';
+const cycleStatusColors = {
+  inProgress: 'bg-theme-yellow-400',
+  completed: 'bg-theme-teal-400',
+  interrupted: 'bg-theme-red-400',
+};
+
+function sortCyclebyMostRecent(cycles: Cycle[]) {
+  return cycles
+    .slice()
+    .sort(
+      (a, b) =>
+        new Date(b.startDate).getTime() - new Date(a.startDate).getTime()
+    );
+}
 
 export default function TableBody() {
   const { cycles } = useCyclesStore().state;
 
-  const content = cycles.map((cycle) => {
+  const orderedCycles = sortCyclebyMostRecent(cycles);
+
+  const content = orderedCycles.map((cycle) => {
+    const status = cycle.finishDate
+      ? 'completed'
+      : cycle.interruptDate
+      ? 'interrupted'
+      : 'inProgress';
+
     return (
       <tr
         key={cycle.id}
         className="group border-t-2 border-theme-gray-900 odd:bg-theme-neutral-800"
       >
-        <td className=" py-2 pl-2 text-theme-gray-200 group-last:rounded-bl">
-          {cycle.task || 'No task assigned to this cycle'}
+        <td className="max-w-2xl px-3 py-2 font-semibold text-theme-gray-200 group-last:rounded-bl">
+          <span className="wrap-text">
+            {cycle.task || 'No task assigned to this cycle'}
+          </span>
         </td>
-        <td className="py-2 pl-2 text-theme-gray-200">
-          {cycle.duration} {cycle.duration > 1 ? 'minutes' : 'minute'}
+        <td className="px-3 py-2 font-semibold text-theme-gray-200">
+          <span>
+            {cycle.duration} {cycle.duration > 1 ? 'minutes' : 'minute'}
+          </span>
         </td>
-        <td className="py-2 pl-2 text-theme-gray-200">
-          {dayjs(new Date(cycle.startDate)).fromNow()}
+        <td className="px-3 py-2 font-semibold text-theme-gray-200">
+          <span>{dayjs(new Date(cycle.startDate)).fromNow()}</span>
         </td>
-        <td className="flex items-center gap-1 py-2 pl-2 text-theme-gray-200 group-last:rounded-br">
-          <div className="h-2 w-2 rounded-full bg-theme-yellow-400" />
-          In progress
+        <td className="px-3 py-2 font-semibold text-theme-gray-200 group-last:rounded-br">
+          <div className="flex items-center gap-1">
+            <div
+              className={`h-2 w-2 rounded-full ${cycleStatusColors[status]}`}
+            />
+            <span>{status === 'inProgress' ? 'in progress' : status}</span>
+          </div>
         </td>
       </tr>
     );
   });
+
   return content;
 }
