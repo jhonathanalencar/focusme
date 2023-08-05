488f11d2e649f43a3fe2ee3b65952de48c3b4a95
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-08-05 19:31:49 -0300

    style: add scrollbar styles

--- src/app/history/components/TableBody.tsx
+++ src/app/history/components/TableBody.tsx
@@ -39,25 +39,29 @@ export default function TableBody() {
         key={cycle.id}
         className="group border-t-2 border-theme-gray-900 odd:bg-theme-neutral-800"
       >
-        <td className="max-w-2xl px-3 py-2 font-semibold text-theme-gray-200 group-last:rounded-bl">
-          <span className="wrap-text">
+        <td className="max-w-2xl px-3 py-2 group-last:rounded-bl">
+          <span className="wrap-text text-sm font-semibold text-theme-gray-200 md:text-base">
             {cycle.task || 'No task assigned to this cycle'}
           </span>
         </td>
-        <td className="px-3 py-2 font-semibold text-theme-gray-200">
-          <span>
+        <td className="px-3 py-2">
+          <span className="text-sm font-semibold text-theme-gray-200 md:text-base">
             {cycle.duration} {cycle.duration > 1 ? 'minutes' : 'minute'}
           </span>
         </td>
-        <td className="px-3 py-2 font-semibold text-theme-gray-200">
-          <span>{dayjs(new Date(cycle.startDate)).fromNow()}</span>
+        <td className="px-3 py-2">
+          <span className="text-sm font-semibold text-theme-gray-200 md:text-base">
+            {dayjs(new Date(cycle.startDate)).fromNow()}
+          </span>
         </td>
-        <td className="px-3 py-2 font-semibold text-theme-gray-200 group-last:rounded-br">
+        <td className="px-3 py-2 group-last:rounded-br">
           <div className="flex items-center gap-1">
             <div
               className={`h-2 w-2 rounded-full ${cycleStatusColors[status]}`}
             />
-            <span>{status === 'inProgress' ? 'in progress' : status}</span>
+            <span className="text-sm font-semibold text-theme-gray-200 md:text-base">
+              {status === 'inProgress' ? 'in progress' : status}
+            </span>
           </div>
         </td>
       </tr>
